82c00817f8d07e7f2e06a4d3b954bfd00a8f3947
MatheusHenriqueFerreira <maatheus27silva@gmail.com>
2022-07-19 15:42:51 -0300

    Definido rota da página Login

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,14 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom';
 //pages
 import { Home } from './pages/Home';
 import { Register } from './pages/Register';
+import { Login } from './pages/Login';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/register' element={<Register/>} />
+      <Route path='/login' element={<Login/>} />
     </Routes>
   </BrowserRouter>
 )
