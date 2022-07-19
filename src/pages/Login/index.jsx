595b6cabba43d35b39fb75127bb3afebf998feb2
MatheusHenriqueFerreira <maatheus27silva@gmail.com>
2022-07-19 16:06:24 -0300

    Inserção de input, button e estilização da página

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -5,9 +5,25 @@ import './styles.css'
 export const Login = () => {
   return(
     <>
-      <Navbar page={'Login'} btnText={'Cadastre-se'} btnLink={'/login'} />
+      <Navbar page={'Login'} btnText={'Cadastre-se'} btnLink={'/register'} />
       <main className="loginMain">
-        
+        <div className="loginContainer1">
+          <h1>Login</h1>
+        </div>
+        <div className="loginContainer2">
+          <div>
+            <label>E-mail</label>
+            <input type={'text'} placeholder='Insira o seu e-mail' />
+          </div>
+          <div>
+            <label>Senha</label>
+            <input type={'password'} />
+            <span>Senha inválida. Por favor, verifique se o endereço de e-mail e/ou senha são válidos</span>
+          </div>
+          <div className="loginBtn">
+            <button type="button">Entrar</button>
+          </div>
+        </div>
       </main>
     </>
   );
