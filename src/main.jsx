7f628617b7f29f13aebed16ce94cf3020bde5d8b
MatheusHenriqueFerreira <maatheus27silva@gmail.com>
2022-07-19 14:26:01 -0300

    Criação da rota da página Register

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/global.css';
-import { Home } from './pages/Home';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
+//pages
+import { Home } from './pages/Home';
+import { Register } from './pages/Register';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Home/>}/>
+      <Route path='/' element={<Home/>} />
+      <Route path='/register' element={<Register/>} />
     </Routes>
   </BrowserRouter>
 )
