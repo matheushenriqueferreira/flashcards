05d4565c07ac16499afd53ff3d59c30b7f546cfd
MatheusHenriqueFerreira <maatheus27silva@gmail.com>
2022-07-21 11:29:03 -0300

    Inserido CSS animation

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,21 @@ import './styles.css'
 import { Navbar } from "../../components/Navbar";
 
 export const Home = () => {
+  
+  const handleClickFlashcard1 = () => {
+    const homeContainer2 = document.querySelector('.homeContainer2');
+    const homeContainer3Yellow = document.querySelector('.homeContainer3Yellow');
+    const whiteArrow = document.querySelector('.fa-right-long');
+    const homeFlashcard1 = document.querySelector('#homeFlashcard1');
+    const homeFlashcard2 = document.querySelector('#homeFlashcard2');
+
+    homeFlashcard1.style.cursor = 'auto';
+    homeContainer2.style.opacity = '0';
+    homeContainer3Yellow.style.opacity = '0';
+    whiteArrow.style.opacity = '1';
+    homeFlashcard2.style.opacity = '1';
+  }
+
   return(
     <>
       <Navbar page={'Home'} />
@@ -15,12 +30,12 @@ export const Home = () => {
           <span>Como se chama carro em inglês?</span>
         </div>
         <div className="homeContainer3">
-          <div className="homeYellow">
+          <div className="homeContainer3Yellow">
             <span>Clique no cartão para descobrir</span>
-            <i className="fa-solid fa-arrow-trend-down"></i>
+            <i className="fa-solid fa-arrow-right"></i>
           </div>
           <div>
-            <div className="homeFlashcards" id="homeFlashcard1">
+            <div className="homeFlashcards" id="homeFlashcard1" onClick={() => handleClickFlashcard1()}>
               <span>Carro</span>
             </div>
             <i className="fa-solid fa-right-long"></i>
