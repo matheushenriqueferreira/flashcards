9e6fd2028f83b3f32e6bb6844fd164ee7b78a608
MatheusHenriqueFerreira <maatheus27silva@gmail.com>
2022-07-21 22:44:44 -0300

    Adicionado um comentário

--- src/firebase/firebase.jsx
+++ src/firebase/firebase.jsx
@@ -2,7 +2,12 @@ import { initializeApp } from "firebase/app";
 import { initializeFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  
+  /*
+
+    Em breve farei um tutorial de como criar o seu 
+    projeto no firebase para utilização dessa aplicação.
+
+  */
 };
 
 export const firebase = initializeApp(firebaseConfig);
