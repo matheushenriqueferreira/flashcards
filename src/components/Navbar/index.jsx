7641169e8764a9cdef9cf763cd2c8cc0020eaa63
MatheusHenriqueFerreira <maatheus27silva@gmail.com>
2022-07-25 10:05:06 -0300

    Correção: Ajustado links da barra de navegação
    Erro estava presente ao entrar nas paginas Register e Login

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import './styles.css'
+import { logout } from "../../redux/userSlice";
 
 export const Navbar = ({ page, btnText, btnLink }) => {
   const navigate = useNavigate();
+  const { userLogged } = useSelector(state=> state.user);
+  const dispatch = useDispatch();
+
   return(
     <header className="navbarHeader">
       <div className="navbarNav">
@@ -13,18 +18,19 @@ export const Navbar = ({ page, btnText, btnLink }) => {
         </div>
         <div className="navbarButtons">
           {
-            page === "Home" ?
+            page === "Register" || page === "Login" ?
+              <button onClick={() => navigate(btnLink)} type="button">{btnText}</button>
+            :
+            page === "Home" && userLogged === 'notLogged' ?
             <>
               <button onClick={() => navigate('/register')} type="button">Cadastre-se</button>
               <button onClick={() => navigate('/login')} type="button">Entrar</button>
             </>
             :
-            page === "Register" || page === "Login" ?
             <>
-              <button onClick={() => navigate(btnLink)} type="button">{btnText}</button>
+              <button onClick={() => navigate('')} type="button">Minhas Coleções</button>
+              <button onClick={() => dispatch(logout())} type="button">Sair</button>
             </>
-            :
-            null
           }
         </div>
       </div>
