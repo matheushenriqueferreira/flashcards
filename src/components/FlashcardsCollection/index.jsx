d82565a8d63da38c74378b01145f3c3a4b05df29
MatheusHenriqueFerreira <maatheus27silva@gmail.com>
2022-07-26 19:24:38 -0300

    Funcionalidade: Componente dinâmico

--- src/components/FlashcardsCollection/index.jsx
+++ src/components/FlashcardsCollection/index.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import './styles.css';
 
 
-export const FlashcardsCollection = () => {
+export const FlashcardsCollection = ({collectionName, collectionImageUrl}) => {
   return(
     <div id="flashcardsCollection">
       <div>
         <i className="fa-solid fa-pencil"></i>
       </div>
       <div>
-        <img className="collectionImage" src={''} alt={'Imagem que ilustra a coleção criada'}/>
+        <img className="collectionImage" src={collectionImageUrl} alt={'Imagem que ilustra a coleção criada'}/>
       </div>
       <div>
-        <span>Nome da Coleção</span>
+        <span>{collectionName}</span>
       </div>
       <div>
-        <i className="fa-regular fa-trash-can"></i>
+        <i className="fa-regular fa-trash-can" data-bs-toggle="modal" data-bs-target="#staticBackdrop"></i>
       </div>
     </div>
   );
