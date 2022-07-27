facda8fea544bf31adbd0ca85802f5f1a2d366d4
MatheusHenriqueFerreira <maatheus27silva@gmail.com>
2022-07-27 20:55:23 -0300

    Definição do estilos
    Inserção do input de busca e botões

--- src/pages/Collection/index.jsx
+++ src/pages/Collection/index.jsx
@@ -13,7 +13,27 @@ export const Collection = () => {
       {
         userLogged === true ?
         <main className="collectionMain">
-
+          <div className="collectionMainTitle">
+            <span>Coleção - Objetos</span>
+          </div>
+          <fildset className="flashcardSearch">
+            <div>
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </div>
+            <div>
+              <input id="inputFlashcardSearch" type={'text'} placeholder={'Busque por um elemento'} />
+            </div>
+          </fildset>
+          <div className="btnNewCard">
+            <button type="button" className="collectionMainBtnStyle">Novo cartão</button>
+          </div>
+          <div className="collectionMainCards">
+            <span>Flashcard 1</span>
+            <span>Flashcard 2</span>
+          </div>
+          <div className="btnPlay">
+            <button type="button" className="collectionMainBtnStyle">Jogar</button>
+          </div>
         </main>
         :
         <Navigate to={'/'} />
