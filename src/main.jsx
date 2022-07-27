38ebbd083ed62c68699aca4763008e0c7f9fc8fe
MatheusHenriqueFerreira <maatheus27silva@gmail.com>
2022-07-27 15:01:51 -0300

    Configurado as rotas para o redux-persist

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/global.css';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import { store } from './redux/store';
+import { store, persistor } from './redux/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 //pages
 import { Home } from './pages/Home';
@@ -11,15 +12,19 @@ import { Register } from './pages/Register';
 import { Login } from './pages/Login';
 import { NewCollection } from './pages/NewCollection'
 
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/register' element={<Register/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/newCollection' element={<NewCollection/>} />
-      </Routes>
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/register' element={<Register/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/newCollection' element={<NewCollection/>} />
+        </Routes>
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 )
