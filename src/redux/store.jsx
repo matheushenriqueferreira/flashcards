626165e249fce2f44bb28155fb9812a640f63609
MatheusHenriqueFerreira <maatheus27silva@gmail.com>
2022-07-27 14:55:10 -0300

    Configurado redux-persist
    Configurado a persistencia para o reducer user

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,10 +1,38 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./userSlice";
 import collectionSlice from "./collectionSlice";
+import refreshPageSlice from "./refreshPageSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, userSlice);
 
 export const store = configureStore({
-  reducer: {
-    user: userSlice,
-    collection: collectionSlice
-  },
+  reducer: { user: persistedReducer, collection: collectionSlice, refreshPage: refreshPageSlice },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
+
+
+
