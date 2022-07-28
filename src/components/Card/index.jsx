76c383dcd5808b2479d743d90331e110ea6860de
MatheusHenriqueFerreira <maatheus27silva@gmail.com>
2022-07-28 14:55:40 -0300

    Funcionalidade: onClick no icone lápis
    Dispatch(editCard) e navigate('/editCard')

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { editCard } from '../../redux/flashcardSlice';
 import './styles.css'
 
 export const Card = ({id, front, back}) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   return(
     <div className="componentCard">
       <div className="componentCardName">
@@ -9,7 +14,10 @@ export const Card = ({id, front, back}) => {
       </div>
       <div className="componentCardBtn">
         <div>
-          <i className="fa-solid fa-pencil"></i>
+          <i onClick={() => {
+            dispatch(editCard({id: id, front: front, back: back})),
+            navigate('/editCard')
+          }} className="fa-solid fa-pencil"></i>
         </div>
         <div>
           <i className="fa-regular fa-trash-can"></i>
