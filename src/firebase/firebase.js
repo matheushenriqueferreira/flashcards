6a5cab71fa1fe884980447ed6a8d373ca642b0eb
MatheusHenriqueFerreira <maatheus27silva@gmail.com>
2022-07-28 18:44:14 -0300

    Inserido config do firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { initializeFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  /*
-
-    Em breve farei um tutorial de como criar o seu 
-    projeto no firebase para utilização dessa aplicação.
-
-  */
+  apiKey: "AIzaSyBiiPQVSNxGE3cPxIFrRfHNy0J_hPk8v8g",
+  authDomain: "flashcardsproject-9fccc.firebaseapp.com",
+  projectId: "flashcardsproject-9fccc",
+  storageBucket: "flashcardsproject-9fccc.appspot.com",
+  messagingSenderId: "108411268019",
+  appId: "1:108411268019:web:43a61e4ed8d966047b39c6"
 };
 
 export const firebase = initializeApp(firebaseConfig);
