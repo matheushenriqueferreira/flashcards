eaf0175dc7c1b4674430b51cfd0727123b67a5a0
MatheusHenriqueFerreira <maatheus27silva@gmail.com>
2022-07-27 21:25:26 -0300

    Inserção do component <Card/> e correção de erro
    Erro de digitação na tag <fieldset/>

--- src/pages/Collection/index.jsx
+++ src/pages/Collection/index.jsx
@@ -3,7 +3,7 @@ import './styles.css';
 import { Navbar } from '../../components/Navbar';
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
-
+import { Card } from "../../components/Card";
 
 export const Collection = () => {
   const { userLogged, userEmail } = useSelector(state => state.user);
@@ -16,23 +16,23 @@ export const Collection = () => {
           <div className="collectionMainTitle">
             <span>Coleção - Objetos</span>
           </div>
-          <fildset className="flashcardSearch">
+          <fieldset className="flashcardSearch">
             <div>
               <i className="fa-solid fa-magnifying-glass"></i>
             </div>
             <div>
               <input id="inputFlashcardSearch" type={'text'} placeholder={'Busque por um elemento'} />
             </div>
-          </fildset>
+          </fieldset>
           <div className="btnNewCard">
             <button type="button" className="collectionMainBtnStyle">Novo cartão</button>
           </div>
           <div className="collectionMainCards">
-            <span>Flashcard 1</span>
-            <span>Flashcard 2</span>
+            <Card />
+            <Card />
           </div>
           <div className="btnPlay">
-            <button type="button" className="collectionMainBtnStyle">Jogar</button>
+            <button type="button" className="collectionMainBtnStyle">Jogar!</button>
           </div>
         </main>
         :
