8bf25532ca45b68414f3dc2efc53e48761996d82
MatheusHenriqueFerreira <maatheus27silva@gmail.com>
2022-07-28 14:40:13 -0300

    Inserido flashcardSlice na store

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./userSlice";
 import collectionSlice from "./collectionSlice";
 import refreshPageSlice from "./refreshPageSlice";
+import flashcardSlice from './flashcardSlice';
 import {
   persistStore,
   persistReducer,
@@ -23,7 +24,7 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, userSlice);
 
 export const store = configureStore({
-  reducer: { user: persistedReducer, collection: collectionSlice, refreshPage: refreshPageSlice },
+  reducer: { user: persistedReducer, collection: collectionSlice, refreshPage: refreshPageSlice, flashcard: flashcardSlice },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
