952ca5d8d237d257bb96403773885feebc1c68bc
MatheusHenriqueFerreira <maatheus27silva@gmail.com>
2022-07-28 14:38:38 -0300

    Criado o  flashcardSlice

---
+++ src/redux/flashcardSlice.jsx
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const flashcardSlice = createSlice({
+  name: flashcard,
+  initialState: {
+    id: '',
+    front: '',
+    back: '',
+  },
+  reducers: {
+    editCard: (state, payload) => {
+      const { id, front, back } = action.payload;
+      return { id: id, front: front, back: back };
+    },
+    deleteCard: (state, payload) => {
+      const { id } = action.payload;
+      return { id: id }
+    }
+  }
+})
+
+export const { editCard, deleteCard } = flashcardSlice.actions;
+export default flashcardSlice.reducer;
