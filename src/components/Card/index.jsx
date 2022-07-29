a95b82535192e553e79d5fdb131f5cbb98d9469e
MatheusHenriqueFerreira <maatheus27silva@gmail.com>
2022-07-29 18:54:48 -0300

    Fix: Correção na estilização dos icones
    No icone de lápis e de lixeira

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { editCard, deleteCard } from '../../redux/flashcardSlice';
-import './styles.css'
+import './styles.css';
 
 export const Card = ({id, front, back}) => {
   const dispatch = useDispatch();
@@ -13,13 +13,13 @@ export const Card = ({id, front, back}) => {
         <span>{front}</span>
       </div>
       <div className="componentCardBtn">
-        <div>
+        <div className="componentCardIconPencil">
           <i onClick={() => {
             dispatch(editCard({id: id, front: front, back: back})),
             navigate('/editCard')
           }} className="fa-solid fa-pencil"></i>
         </div>
-        <div>
+        <div className="componentCardIconTrash">
           <i onClick={() => dispatch(deleteCard({id: id}))} data-bs-toggle="modal" data-bs-target="#staticBackdrop" className="fa-regular fa-trash-can"></i>
         </div>
       </div>
