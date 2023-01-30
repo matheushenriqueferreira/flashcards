ea396185c14bd1ed0df9278d1ebce77b24fbf041
matheushenriqueferreira <maatheus27silva@gmail.com>
2023-01-30 20:28:26 -0300

    Removido configurações obsoletas

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { initializeFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBiiPQVSNxGE3cPxIFrRfHNy0J_hPk8v8g",
-  authDomain: "flashcardsproject-9fccc.firebaseapp.com",
-  projectId: "flashcardsproject-9fccc",
-  storageBucket: "flashcardsproject-9fccc.appspot.com",
-  messagingSenderId: "108411268019",
-  appId: "1:108411268019:web:43a61e4ed8d966047b39c6"
+  // Insira aqui a configuração do projeto do Firebase
 };
 
 export const firebase = initializeApp(firebaseConfig);
